1e2e5346982f1c4fe4cb212343fde20686f27284
Qusai Fares <qusaifares@gmail.com>
2020-02-28 12:14:22 -0500

    Updates

---
+++ precache-manifest.a95c2a7c9d67aaec56c2bed008c600f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c9a5909acd06753491f4173e9021197",
+    "url": "/tarneeb/index.html"
+  },
+  {
+    "revision": "ac0fb17b6c3fc30cb5d1",
+    "url": "/tarneeb/static/css/main.a29d6e4a.chunk.css"
+  },
+  {
+    "revision": "c7448b1557a3fbaf87ae",
+    "url": "/tarneeb/static/js/2.dea2a224.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/tarneeb/static/js/2.dea2a224.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac0fb17b6c3fc30cb5d1",
+    "url": "/tarneeb/static/js/main.dc62e1c4.chunk.js"
+  },
+  {
+    "revision": "aa5b80bbf26f13b1b230",
+    "url": "/tarneeb/static/js/runtime-main.7f4546bc.js"
+  }
+]);
